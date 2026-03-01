4ced3e5f800672050a09f2dda92f4299bccf1be3
awallace689 <awallace689@gmail.com>
2026-03-01 13:32:00 -0600

    fix pp hook for non-main

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Mta.MixProject do
 
   def aliases do
     [
-      pp: ["cmd .git/hooks/pre-push"],
+      pp: ["cmd --shell CI=true .git/hooks/pre-push"],
       t: fn args ->
         Mix.Task.run("test", ["--cover", "--slowest", "3"] ++ args)
       end,
